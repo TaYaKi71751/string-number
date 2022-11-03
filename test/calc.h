5d05702db066f601f6d7da6908f38120a183ee84
TaYaKi71751 <shantian.tw@gmail.com>
2022-11-03 18:01:14 +0900

    Add `test/calc.{c,h}`

---
+++ test/calc.h
@@ -0,0 +1,19 @@
+#define __CUSTOM_INTEGER_DEF_SIGN_MINUS__() ("-")
+#define __CUSTOM_INTEGER_DEF_SIGN_PLUS__() ("")
+#define __CUSTOM_INTEGER_DEF_NUMBER_CHARSET__() ("0123456789")
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+#include<stdbool.h>
+#include<string.h>
+
+#include "../include/integer_def_sign.h"
+#include "../include/integer_def_number.h"
+#include "../include/integer_def_calc.h"
+
+#ifndef __CUSTOM_TEST_CALC_H__
+#define __CUSTOM_TEST_CALC_H__
+
+
+#endif
