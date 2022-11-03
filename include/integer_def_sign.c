62d6eaa0a2fd9b927fb5e08de8ae2d9280a57f73
TaYaKi71751 <shantian.tw@gmail.com>
2022-11-03 17:44:55 +0900

    Fix `__IS_SIGN_{NEGATIVE,POSITIVE,NULL}__` `(sign)` => `(sign_index,sign_length,sign)`

--- include/integer_def_sign.c
+++ include/integer_def_sign.c
@@ -1,29 +1,36 @@
 #include "./integer_def_sign.h"
 
-bool __IS_SIGN_NEGATIVE__(char* sign){
+bool __IS_SIGN_NEGATIVE__(size_t sign_index,size_t sign_length,char* sign){
  return (
-  strlen_runtime(__SIGN_NEGATIVE__()) &&
-  strlen_runtime(sign) &&
-  strlen_runtime(sign) >= strlen_runtime(__SIGN_NEGATIVE__()) ? (
-   strncmp(__SIGN_NEGATIVE__(),sign,strlen_runtime(__SIGN_NEGATIVE__())) == 0
-  ) : false
+	sign_length && 
+	sign_index < sign_length &&
+	strlen_runtime(__SIGN_NEGATIVE__()) ? (
+	 sign_index < strlen_runtime(__SIGN_NEGATIVE__()) ? (
+		__SIGN_NEGATIVE__()[sign_index] == sign[sign_index] ?
+		 true :
+		 __IS_SIGN_NEGATIVE__(sign_index + 1,sign_length,sign)
+	 ) : __IS_SIGN_NEGATIVE__(sign_index + 1,sign_length,sign)
+	) : false
  );
 }
 
-bool __IS_SIGN_POSITIVE__(char* sign){
+bool __IS_SIGN_POSITIVE__(size_t sign_index,size_t sign_length,char* sign){
  return (
-  strlen_runtime(__SIGN_PLUS__()) &&
-  strlen_runtime(sign) &&
-  strlen_runtime(sign) >= strlen_runtime(__SIGN_POSITIVE__()) ? (
-   strncmp(__SIGN_POSITIVE__(),sign,strlen_runtime(__SIGN_POSITIVE__())) == 0
-  ) : false
+	sign_length && 
+	sign_index < sign_length &&
+	strlen_runtime(__SIGN_POSITIVE__()) ? (
+	 sign_index < strlen_runtime(__SIGN_POSITIVE__()) ? (
+		__SIGN_POSITIVE__()[sign_index] == sign[sign_index] ?
+		 true :
+		 __IS_SIGN_POSITIVE__(sign_index + 1,sign_length,sign)
+	 ) : __IS_SIGN_POSITIVE__(sign_index + 1,sign_length,sign)
+	) : false
  );
 }
 
-bool __IS_SIGN_NULL__(char* sign){
- if(strlen_runtime(sign) <= strlen_runtime(__SIGN_NEGATIVE__()) && strlen_runtime(sign) <= strlen_runtime(__SIGN_POSITIVE__())) return true;
+bool __IS_SIGN_NULL__(size_t sign_index,size_t sign_length,char* sign){
  return (
-  !__IS_SIGN_NEGATIVE__(sign) ||
-  !__IS_SIGN_POSITIVE__(sign)
+	!__IS_SIGN_NEGATIVE__(sign_index,sign_length,sign) &&
+	!__IS_SIGN_POSITIVE__(sign_index,sign_length,sign)
  );
 }
