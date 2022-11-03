3c5b95e2488372143ff8d6f4446b20345bb4fae2
TaYaKi71751 <shantian.tw@gmail.com>
2022-11-03 18:01:14 +0900

    Add `test/calc.{c,h}`

---
+++ test/calc.c
@@ -0,0 +1,90 @@
+#include "./calc.h"
+
+#define __STRING_END_NULL_BYTE_SIZE__ 1
+
+int __TEST_ADD_INTEGER_LOOP__(){
+ int _r = 0x00;
+ char *a = 0x00,*b = 0x00,*r = 0x00,*_rtoa = 0x00;
+ for(int i = (((int)__UINT8_MAX__) * -1);i < ((int)__UINT8_MAX__);i++){
+  for(int j = (((int)__UINT8_MAX__) * -1);j < ((int)__UINT8_MAX__);j++){
+   _r = i + j;
+   _rtoa = itoa(_r);
+   a = itoa(i);
+   b = itoa(j);
+   r = __ADD_INTEGER__(a,b);
+   if(strcmp(r,_rtoa) != 0)(
+    printf("\"%s\",\"%s\"",r,_rtoa),
+    abort()
+   );
+   (free(a),free(b),a = 0x00,b = 0x00);
+   (free(r),free(_rtoa),r = 0x00,_rtoa = 0x00);
+  }
+ }
+ puts("OK with __TEST_ADD_INTEGER_LOOP__");
+ return 0;
+}
+
+int __TEST_SUB_INTEGER_LOOP__(){
+ int _r = 0x00;
+ char *a = 0x00,*b = 0x00,*r = 0x00,*_rtoa = 0x00;
+ for(int i = (((int)__UINT8_MAX__) * -1);i < ((int)__UINT8_MAX__);i++){
+  for(int j = (((int)__UINT8_MAX__) * -1);j < ((int)__UINT8_MAX__);j++){
+   _r = i - j;
+   _rtoa = itoa(_r);
+   a = itoa(i);
+   b = itoa(j);
+   r = __SUB_INTEGER__(a,b);
+   if(strcmp(r,_rtoa) != 0)(
+    printf("\"%s\",\"%s\"",r,_rtoa),
+    abort()
+   );
+   (free(a),free(b),a = 0x00,b = 0x00);
+   (free(r),free(_rtoa),r = 0x00,_rtoa = 0x00);
+  }
+ }
+ puts("OK with __TEST_SUB_INTEGER_LOOP__");
+ return 0;
+}
+
+int __TEST_ADD_INTEGER__(long _a,long _b){
+ long _r = 0x00;
+ size_t input_size = 0x00;
+ char *a = 0x00,*b = 0x00,*r = 0x00,*_rtoa = 0x00;
+ size_t a_length = 0x00,b_length = 0x00;
+ _r = _a + _b;
+ _rtoa = ltoa(_r);
+ a = ltoa(_a);
+ b = ltoa(_b);
+ r = __ADD_INTEGER__(a,b);
+ if(strcmp(r,_rtoa) != 0)(
+  printf("\"%s\",\"%s\"",r,_rtoa),
+  abort()
+ );
+ (free(a),free(b),a = 0x00,b = 0x00);
+ (free(r),free(_rtoa),r = 0x00,_rtoa = NULL);
+ puts("OK with __TEST_ADD_INTEGER__");
+}
+
+int __TEST_SUB_INTEGER__(long _a,long _b){
+ long _r = 0x00;
+ size_t input_size = 0x00;
+ char *a = 0x00,*b = 0x00,*r = 0x00,*_rtoa = 0x00;
+ size_t a_length = 0x00,b_length = 0x00;
+ _r = _a - _b;
+ _rtoa = ltoa(_r);
+ a = ltoa(_a);
+ b = ltoa(_b);
+ r = __SUB_INTEGER__(a,b);
+ if(strcmp(r,_rtoa) != 0)(
+  printf("\"%s\",\"%s\"",r,_rtoa),
+  abort()
+ );
+ (free(a),free(b),a = 0x00,b = 0x00);
+ (free(r),free(_rtoa),r = 0x00,_rtoa = NULL);
+ puts("OK with __TEST_SUB_INTEGER__");
+}
+
+int main(){
+ __TEST_SUB_INTEGER_LOOP__();
+ __TEST_ADD_INTEGER_LOOP__();
+}
