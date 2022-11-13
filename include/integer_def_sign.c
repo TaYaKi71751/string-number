17b293a920516a9504a45849bb896f0631fa846d
TaYaKi71751 <shantian.tw@gmail.com>
2022-11-13 18:03:29 +0900

    Fix `__IS_SIGN_{NEGATIVE,POSITIVE}__` to use `strncmp`

--- include/integer_def_sign.c
+++ include/integer_def_sign.c
@@ -3,13 +3,12 @@
 bool __IS_SIGN_NEGATIVE__(size_t sign_index,size_t sign_length,char* sign){
  return (
 	sign_length && 
-	sign_index < sign_length &&
-	strlen_runtime(__SIGN_NEGATIVE__()) ? (
-	 sign_index < strlen_runtime(__SIGN_NEGATIVE__()) ? (
-		__SIGN_NEGATIVE__()[sign_index] == sign[sign_index] ?
+	strlen_runtime(__SIGN_NEGATIVE__()) &&
+	(sign_index + strlen_runtime(__SIGN_NEGATIVE__())) <= sign_length &&
+	 sign_index <= strlen_runtime(__SIGN_NEGATIVE__()) ? (
+		strncmp(__SIGN_NEGATIVE__(),sign + sign_index,strlen_runtime(__SIGN_NEGATIVE__())) == 0 ?
 		 true :
 		 __IS_SIGN_NEGATIVE__(sign_index + 1,sign_length,sign)
-	 ) : __IS_SIGN_NEGATIVE__(sign_index + 1,sign_length,sign)
 	) : false
  );
 }
@@ -17,13 +16,12 @@ bool __IS_SIGN_NEGATIVE__(size_t sign_index,size_t sign_length,char* sign){
 bool __IS_SIGN_POSITIVE__(size_t sign_index,size_t sign_length,char* sign){
  return (
 	sign_length && 
-	sign_index < sign_length &&
-	strlen_runtime(__SIGN_POSITIVE__()) ? (
-	 sign_index < strlen_runtime(__SIGN_POSITIVE__()) ? (
-		__SIGN_POSITIVE__()[sign_index] == sign[sign_index] ?
+	strlen_runtime(__SIGN_POSITIVE__()) &&
+	(sign_index + strlen_runtime(__SIGN_POSITIVE__())) <= sign_length &&
+	sign_index <= strlen_runtime(__SIGN_POSITIVE__()) ? (
+		strncmp(__SIGN_POSITIVE__(),sign + sign_index,strlen_runtime(__SIGN_POSITIVE__())) == 0 ?
 		 true :
 		 __IS_SIGN_POSITIVE__(sign_index + 1,sign_length,sign)
-	 ) : __IS_SIGN_POSITIVE__(sign_index + 1,sign_length,sign)
 	) : false
  );
 }
